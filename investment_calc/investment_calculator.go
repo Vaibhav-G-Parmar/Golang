789eb3200291b59b7a6133c746b076cf24b81d6a
Vaibhav-G-Parmar <vaibhavgirish2093@gmail.com>
2025-11-30 14:03:06 -0500

    ask user for investment details

--- investment_calc/investment_calculator.go
+++ investment_calc/investment_calculator.go
@@ -7,13 +7,23 @@ import (
 
 func main() {
 	const inflationRate = 2.5
-	var investmentAmount = 1000
-	var expectedReturnRate = 5.5
-	var years = 10
+	var investmentAmount float64
+	var expectedReturnRate float64
+	var years float64
 	//var years float64 = 10
 
-	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
-	var futureRealValue = futureValue * math.Pow(1 + inflationRate / 100, float64(years))
+	fmt.Print("Please enter the Investment Amount: ")
+	fmt.Scan(&investmentAmount)
+	//for only one words input - Scan() cannot take multiline input
+
+	fmt.Print("Please enter the Expected Return Rate: ")
+	fmt.Scan(&expectedReturnRate)
+	
+	fmt.Print("Please enter the years: ")
+	fmt.Scan(&years)
+
+	var futureValue = investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
+	var futureRealValue = futureValue * math.Pow(1 + inflationRate / 100, years)
 
 	fmt.Println(futureValue)
 	fmt.Println(futureRealValue)
