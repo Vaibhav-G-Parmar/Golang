6a4bb99bca0530e8122933139f2d1d738225672d
Vaibhav-G-Parmar <vaibhavgirish2093@gmail.com>
2025-11-30 13:43:29 -0500

    including inflation rate in the calculation

--- investment_calc/investment_calculator.go
+++ investment_calc/investment_calculator.go
@@ -6,11 +6,15 @@ import (
 ) 
 
 func main() {
+	const inflationRate = 2.5
 	var investmentAmount = 1000
 	var expectedReturnRate = 5.5
 	var years = 10
+	//var years float64 = 10
 
 	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
+	var futureRealValue = futureValue * math.Pow(1 + inflationRate / 100, float64(years))
 
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
